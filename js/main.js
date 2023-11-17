fb0be03531e5a74b804b567ee311e97c66fdde2c
Manucharyand <manucharyand@gmail.com>
2023-11-17 00:34:47 -0700

    fixed pictures of cards not showing on HTML. added function to deal with Aces

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 //------------------------------Constants-----------------------------------------------------//
-const turn = {
+let turn = {
     "1": "Player",
     "-1": "Dealer",
     "0" : ""
@@ -10,64 +10,65 @@ const ranks = ["02", "03", "04", "05", "06", "07", "08", "09", "10", "J", "Q", "
 const unshuffledDeck = createDeck();
 
 const imageLinks = {
-    "s02": "/home/manucharyand/BlackJack-Project/css/card-deck-css/images/spades/spades-r02.svg",
-    "s03": "/home/manucharyand/BlackJack-Project/css/card-deck-css/images/spades/spades-r03.svg",
-    "s04": "/home/manucharyand/BlackJack-Project/css/card-deck-css/images/spades/spades-r04.svg",
-    "s05": "/home/manucharyand/BlackJack-Project/css/card-deck-css/images/spades/spades-r05.svg",
-    "s06": "/home/manucharyand/BlackJack-Project/css/card-deck-css/images/spades/spades-r06.svg",
-    "s07": "/home/manucharyand/BlackJack-Project/css/card-deck-css/images/spades/spades-r07.svg",
-    "s08": "/home/manucharyand/BlackJack-Project/css/card-deck-css/images/spades/spades-r08.svg",
-    "s09": "/home/manucharyand/BlackJack-Project/css/card-deck-css/images/spades/spades-r09.svg",
-    "s10": "/home/manucharyand/BlackJack-Project/css/card-deck-css/images/spades/spades-r10.svg",
-    "sJ": "/home/manucharyand/BlackJack-Project/css/card-deck-css/images/spades/spades-J.svg",
-    "sQ": "/home/manucharyand/BlackJack-Project/css/card-deck-css/images/spades/spades-Q.svg",
-    "sK": "/home/manucharyand/BlackJack-Project/css/card-deck-css/images/spades/spades-K.svg",
-    "sA": "/home/manucharyand/BlackJack-Project/css/card-deck-css/images/spades/spades-A.svg",
-    "d02": "/home/manucharyand/BlackJack-Project/css/card-deck-css/images/diamonds/diamonds-r02.svg",
-    "d03": "/home/manucharyand/BlackJack-Project/css/card-deck-css/images/diamonds/diamonds-r03.svg",
-    "d04": "/home/manucharyand/BlackJack-Project/css/card-deck-css/images/diamonds/diamonds-r04.svg",
-    "d05": "/home/manucharyand/BlackJack-Project/css/card-deck-css/images/diamonds/diamonds-r05.svg",
-    "d06": "/home/manucharyand/BlackJack-Project/css/card-deck-css/images/diamonds/diamonds-r06.svg",
-    "d07": "/home/manucharyand/BlackJack-Project/css/card-deck-css/images/diamonds/diamonds-r07.svg",
-    "d08": "/home/manucharyand/BlackJack-Project/css/card-deck-css/images/diamonds/diamonds-r08.svg",
-    "d09": "/home/manucharyand/BlackJack-Project/css/card-deck-css/images/diamonds/diamonds-r09.svg",
-    "d10": "/home/manucharyand/BlackJack-Project/css/card-deck-css/images/diamonds/diamonds-r10.svg",
-    "dJ": "/home/manucharyand/BlackJack-Project/css/card-deck-css/images/diamonds/diamonds-J.svg",
-    "dQ": "/home/manucharyand/BlackJack-Project/css/card-deck-css/images/diamonds/diamonds-Q.svg",
-    "dK": "/home/manucharyand/BlackJack-Project/css/card-deck-css/images/diamonds/diamonds-K.svg",
-    "dA": "/home/manucharyand/BlackJack-Project/css/card-deck-css/images/diamonds/diamonds-A.svg",
-    "c02": "/home/manucharyand/BlackJack-Project/css/card-deck-css/images/clubs/clubs-r02.svg",
-    "c03": "/home/manucharyand/BlackJack-Project/css/card-deck-css/images/clubs/clubs-r03.svg",
-    "c04": "/home/manucharyand/BlackJack-Project/css/card-deck-css/images/clubs/clubs-r04.svg",
-    "c05": "/home/manucharyand/BlackJack-Project/css/card-deck-css/images/clubs/clubs-r05.svg",
-    "c06": "/home/manucharyand/BlackJack-Project/css/card-deck-css/images/clubs/clubs-r06.svg",
-    "c07": "/home/manucharyand/BlackJack-Project/css/card-deck-css/images/clubs/clubs-r07.svg",
-    "c08": "/home/manucharyand/BlackJack-Project/css/card-deck-css/images/clubs/clubs-r08.svg",
-    "c09": "/home/manucharyand/BlackJack-Project/css/card-deck-css/images/clubs/clubs-r09.svg",
-    "c10": "/home/manucharyand/BlackJack-Project/css/card-deck-css/images/clubs/clubs-r10.svg",
-    "cJ": "/home/manucharyand/BlackJack-Project/css/card-deck-css/images/clubs/clubs-J.svg",
-    "cQ": "/home/manucharyand/BlackJack-Project/css/card-deck-css/images/clubs/clubs-Q.svg",
-    "cK": "/home/manucharyand/BlackJack-Project/css/card-deck-css/images/clubs/clubs-K.svg",
-    "cA": "/home/manucharyand/BlackJack-Project/css/card-deck-css/images/clubs/clubs-A.svg",
-    "h02": "/home/manucharyand/BlackJack-Project/css/card-deck-css/images/hearts/hearts-r02.svg",
-    "h03": "/home/manucharyand/BlackJack-Project/css/card-deck-css/images/hearts/hearts-r03.svg",
-    "h04": "/home/manucharyand/BlackJack-Project/css/card-deck-css/images/hearts/hearts-r04.svg",
-    "h05": "/home/manucharyand/BlackJack-Project/css/card-deck-css/images/hearts/hearts-r05.svg",
-    "h06": "/home/manucharyand/BlackJack-Project/css/card-deck-css/images/hearts/hearts-r06.svg",
-    "h07": "/home/manucharyand/BlackJack-Project/css/card-deck-css/images/hearts/hearts-r07.svg",
-    "h08": "/home/manucharyand/BlackJack-Project/css/card-deck-css/images/hearts/hearts-r08.svg",
-    "h09": "/home/manucharyand/BlackJack-Project/css/card-deck-css/images/hearts/hearts-r09.svg",
-    "h10": "/home/manucharyand/BlackJack-Project/css/card-deck-css/images/hearts/hearts-r10.svg",
-    "hJ": "/home/manucharyand/BlackJack-Project/css/card-deck-css/images/hearts/hearts-J.svg",
-    "hQ": "/home/manucharyand/BlackJack-Project/css/card-deck-css/images/hearts/hearts-Q.svg",
-    "hK": "/home/manucharyand/BlackJack-Project/css/card-deck-css/images/hearts/hearts-K.svg",
-    "hA": "/home/manucharyand/BlackJack-Project/css/card-deck-css/images/hearts/hearts-A.svg"
+    "s02": "/css/card-deck-css/images/spades/spades-r02.svg",
+    "s03": "/css/card-deck-css/images/spades/spades-r03.svg",
+    "s04": "/css/card-deck-css/images/spades/spades-r04.svg",
+    "s05": "/css/card-deck-css/images/spades/spades-r05.svg",
+    "s06": "/css/card-deck-css/images/spades/spades-r06.svg",
+    "s07": "/css/card-deck-css/images/spades/spades-r07.svg",
+    "s08": "/css/card-deck-css/images/spades/spades-r08.svg",
+    "s09": "/css/card-deck-css/images/spades/spades-r09.svg",
+    "s10": "/css/card-deck-css/images/spades/spades-r10.svg",
+    "sJ": "/css/card-deck-css/images/spades/spades-J.svg",
+    "sQ": "/css/card-deck-css/images/spades/spades-Q.svg",
+    "sK": "/css/card-deck-css/images/spades/spades-K.svg",
+    "sA": "/css/card-deck-css/images/spades/spades-A.svg",
+    "d02": "/css/card-deck-css/images/diamonds/diamonds-r02.svg",
+    "d03": "/css/card-deck-css/images/diamonds/diamonds-r03.svg",
+    "d04": "/css/card-deck-css/images/diamonds/diamonds-r04.svg",
+    "d05": "/css/card-deck-css/images/diamonds/diamonds-r05.svg",
+    "d06": "/css/card-deck-css/images/diamonds/diamonds-r06.svg",
+    "d07": "/css/card-deck-css/images/diamonds/diamonds-r07.svg",
+    "d08": "/css/card-deck-css/images/diamonds/diamonds-r08.svg",
+    "d09": "/css/card-deck-css/images/diamonds/diamonds-r09.svg",
+    "d10": "/css/card-deck-css/images/diamonds/diamonds-r10.svg",
+    "dJ": "/css/card-deck-css/images/diamonds/diamonds-J.svg",
+    "dQ": "/css/card-deck-css/images/diamonds/diamonds-Q.svg",
+    "dK": "/css/card-deck-css/images/diamonds/diamonds-K.svg",
+    "dA": "/css/card-deck-css/images/diamonds/diamonds-A.svg",
+    "c02": "/css/card-deck-css/images/clubs/clubs-r02.svg",
+    "c03": "/css/card-deck-css/images/clubs/clubs-r03.svg",
+    "c04": "/css/card-deck-css/images/clubs/clubs-r04.svg",
+    "c05": "/css/card-deck-css/images/clubs/clubs-r05.svg",
+    "c06": "/css/card-deck-css/images/clubs/clubs-r06.svg",
+    "c07": "/css/card-deck-css/images/clubs/clubs-r07.svg",
+    "c08": "/css/card-deck-css/images/clubs/clubs-r08.svg",
+    "c09": "/css/card-deck-css/images/clubs/clubs-r09.svg",
+    "c10": "/css/card-deck-css/images/clubs/clubs-r10.svg",
+    "cJ": "/css/card-deck-css/images/clubs/clubs-J.svg",
+    "cQ": "/css/card-deck-css/images/clubs/clubs-Q.svg",
+    "cK": "/css/card-deck-css/images/clubs/clubs-K.svg",
+    "cA": "/css/card-deck-css/images/clubs/clubs-Aa.svg",
+    "h02": "/css/card-deck-css/images/hearts/hearts-r02.svg",
+    "h03": "/css/card-deck-css/images/hearts/hearts-r03.svg",
+    "h04": "/css/card-deck-css/images/hearts/hearts-r04.svg",
+    "h05": "/css/card-deck-css/images/hearts/hearts-r05.svg",
+    "h06": "/css/card-deck-css/images/hearts/hearts-r06.svg",
+    "h07": "/css/card-deck-css/images/hearts/hearts-r07.svg",
+    "h08": "/css/card-deck-css/images/hearts/hearts-r08.svg",
+    "h09": "/css/card-deck-css/images/hearts/hearts-r09.svg",
+    "h10": "/css/card-deck-css/images/hearts/hearts-r10.svg",
+    "hJ": "/css/card-deck-css/images/hearts/hearts-J.svg",
+    "hQ": "/css/card-deck-css/images/hearts/hearts-Q.svg",
+    "hK": "/css/card-deck-css/images/hearts/hearts-K.svg",
+    "hA": "/css/card-deck-css/images/hearts/hearts-A.svg"
 }
 
 
 
 //------------------------------State Variables-----------------------------------------------//
-let playerHand, dealerHand, winner, playerBlackjack, dealerBlackjack, playerTotal, dealerTotal
+let playerHand, dealerHand, winner, playerBlackjack, dealerBlackjack, dealerHiddenCard
+
 
 let shuffledDeck = shuffleDeck();
 
@@ -89,10 +90,16 @@ const doubleEle = document.getElementById("double-btn");
 
 
 //------------------------------Event Listeners-----------------------------------------------//
-hitEle.addEventListener("click", function(){
-    const newPlayerCard = playerCardsEle.appendChild(document.createElement("div"))
-    newPlayerCard.innerHTML = shuffleDeck[4];
-    playerTotal = playerTotal + newPlayerCard.value
+hitEle.addEventListener("click", function(event){
+    event.preventDefault();
+    const newCardEle = document.createElement("div");
+    const newCard = shuffledDeck.shift();
+    
+    newCardEle.innerHTML = `<img src="${imageLinks[newCard.face]}" alt="${newCard.face}">`;
+    playerTotal += newCard.value;
+    playerCardsEle.appendChild(newCardEle);
+    // render();
+    
     if (playerTotal > 21) {
         winner = "-1";
     } else {
@@ -100,11 +107,27 @@ hitEle.addEventListener("click", function(){
     }
 })
 
-standEle.addEventListener("click", function(){
-    turn = "-1";
+standEle.addEventListener("click", function(event){
+    event.preventDefault();
+    const newDealerCardEle = document.createElement("div");
+    const newDealerCard = shuffledDeck.shift();
+    
+    newDealerCardEle.innerHTML = `<img src="${imageLinks[newDealerCard.face]}" alt="${newDealerCard.face}">`;
+    
+        while (dealerTotal < 17) {
+            dealerTotal += newDealerCard.value;
+            dealerCardsEle.appendChild(newDealerCardEle);
+            
+            // render();
+        }
+        if (dealerTotal > 21) {
+            winner = "1";
+        }
 })
 
 
+let playerTotal
+let  dealerTotal 
 
 //------------------------------Functions-----------------------------------------------------//
 //Creates an unshuffled deck. Sets the value of each card to their number value, unless its an Ace, jack, queen, or king.
@@ -115,7 +138,7 @@ function createDeck () {
             let value;
             if (rank ==="A") {
                 value = 11;
-            } else if (rank ==="J" || rank === "Q" || rank ==="K") {
+            } else if (rank === "J" || rank === "Q" || rank === "K") {
                 value = 10;
             } else {
                 value = Number(rank);
@@ -141,44 +164,55 @@ function shuffleDeck () {
 }
 
 
-function calculateScore () {
-    if (dealerTotal < 22) {
-        if (dealerTotal === playerTotal) {
-            winner = "T";
-        } else if (dealerTotal > playertotal) {
-            winner = "-1";
-        } else {
-            winner = "1";
+function calculateWinner () {
+    while (winner === null) {
+        if (dealerTotal < 22) {
+            if (dealerTotal === playerTotal) {
+                winner = "T";
+            } else if (dealerTotal > playerTotal) {
+                winner = "-1";
+            } else {
+                winner = "1";
+            }
         }
     }
 }
 
-function calculateTotal () {
-    playerTotal = playerCard1Ele.value + playerCard2Ele.value
-    dealerTotal = dealerCard1Ele.value + dealerCard2Ele.value
-    if (playerTotal > 21) {
-        winner = "-1";
-    } else if (playerTotal <22) {
-        turn = "1";
-    }
+function calculateTotal (hand) {
+    let total = 0
+    let aces = 0;
     
+    hand.forEach(function (card) {
+        total += card.value;
+        if (card.value === 11) {
+            aces += 1;
+        }
+    })
+    
+//if there is an ace in the hand and the total goes over 21, count the ace as 11 and decrease ace count by 1
+    while (aces > 0 && total > 21) {
+        total -= 10;
+        aces -= 1;
+    }
+    return total;
 }
 
 function checkBlackjack () {
-    if (playerCard1Ele === "10" || "J" || "Q" || "K"  && playerCard2Ele === "A") {
+    if (playerCardsEle[0].value === "10" || playerCardsEle[0].value === "J" || playerCardsEle[0].value === "Q" || playerCardsEle[0].value === "K"  && playerCardsEle[1].value === "A") {
         playerBlackjack = true;
-    } else if (playerCard1Ele === "A"  && playerCard2Ele === "10" || "J" || "Q" || "K") {
+    } else if (playerCardsEle[0].value === "A"  && playerCardsEle[1].value === "10" || playerCardsEle[1].value === "J" || playerCardsEle[1].value === "Q" || playerCardsEle[1].value === "K") {
         playerBlackjack = true;
     }
 
-    if (dealerCard1Ele === "10" || "J" || "Q" || "K" && playerCard2Ele ==="A") {
+    if (dealerCardsEle[0].value === "10" || dealerCardsEle[0].value === "J" || dealerCardsEle[0].value === "Q" || dealerCardsEle[0].value === "K" && dealerCardsEle[1].value ==="A") {
         dealerBlackjack = true;
-    } else if (dealerCard1Ele === "A" && playerCard2Ele === "10" || "J" || "Q" || "K" ) {
+    } else if (dealerCardsEle[0] === "A" && dealerCardsEle[1].value === "10" || dealerCardsEle[1].value === "J" || dealerCardsEle[1].value === "Q" || dealerCardsEle[1].value === "K" ) {
         dealerBlackjack = true;
     }
+
     if (playerBlackjack) {
         if (dealerBlackjack = true) {
-            "You both have blackjack. Push"
+             winner = "T" 
         } else if (dealerBlackjack = false) {
             winner = 1;
         } else {
@@ -189,35 +223,56 @@ function checkBlackjack () {
 
 function render () {
 renderWinner();
-renderDeck();
-renderDealerHand();
-renderPlayerHand();
 }
+
 
 function renderWinner () {
+    const winnerDiv = document.createElement("div");
+
+    if ( winner === "1") {
+        winnerDiv.innerText = "You win!" 
+    } else if  (winner === "-1") {
+        winnerDiv.innerText = "Dealer Wins..." 
+    } else if ( winner === "T") {
+        winnerDiv.innerText = "Its a push." 
+    }
+
+    headerEle.appendChild(winnerDiv);
 
 }
 
-function renderDealerHand () {
-    dealerCard1Ele.innerText = `${shuffledDeck[1].face}`;
-    dealerCard2Ele.innerHTML = `<img src="${imageLinks[shuffledDeck[3].face]}" alt = "${shuffledDeck[3].face}"}>`;
 
-}
+function dealCards(handEle, isFaceDown = false) {
+    const card = shuffledDeck.shift();
+    const cardEle = document.createElement("div");
 
-function renderPlayerHand () {
-    playerCard1Ele.innerHTML = `${shuffledDeck[0].face}`;
-    playerCard2Ele.innerHTML = `${shuffledDeck[2].face}`;
+    if (isFaceDown) {
+        cardEle.innerHTML = `${card.back}`;
+    } else {
+        cardEle.innerHTML = `<img src="${imageLinks[card.face]}" alt="${card.face}">`;
+    }
 
+    handEle.appendChild(cardEle);
+
+    if (handEle === playerCardsEle) {
+        playerTotal += card.value;
+    } else if (handEle === dealerCardsEle) {
+        dealerTotal += card.value;
+    }
 }
 
 
 function init() {
     createDeck();
     shuffleDeck();
-    renderPlayerHand();
-    renderDealerHand();
+    dealCards(playerCardsEle);
+    dealCards(dealerCardsEle);
+    dealCards(playerCardsEle);
+    dealCards(dealerCardsEle);
+    render();
+    turn = "1";
 }
 
 
 
-
+init();
